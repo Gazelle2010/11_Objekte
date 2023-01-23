e37c20a737d0d75868a29f54ad1295cea0d2f6fe
Gazelle2010 <martin-schwerdtner@t-online.de>
2023-01-23 12:11:39 +0100

    Update 2 Hierarchien

--- script_01.js
+++ script_01.js
@@ -34,39 +34,57 @@
 
 /***** Objekte 1 Daten/Funktionen *******/
 
-let person = {
-                        firstName:"Yunaisy",
-                        familyName:"Jug",
-                        salary: [120000,160000],
-                        permission: true,
-                        sayHello:
-                                function(){
-                                        return "Hello, ich bin " + this.firstName + "!";
-                                }
+// let person = {
+//                         firstName:"Yunaisy",
+//                         familyName:"Jug",
+//                         salary: [120000,160000],
+//                         permission: true,
+//                         sayHello:
+//                                 function(){
+//                                         return "Hello, ich bin " + this.firstName + "!";
+//                                 }
 
-};
+// };
 
 // output(person);
 // output(person.firstName); // Dot-Syntax
 // output(person.permission); // Dot-Syntax
 // output(person.salary[1]); // Dot-Syntax
 
-person.familyName = "Jug";
-output(person.sayHello());
+// person.familyName = "Jug";
+// output(person.sayHello());
 
-const txt =     "Ich bin " + person.firstName + " " +
-                person.familyName +
-                " und verdiene " + 
-                person.salary[1] + " p.a";
+// const txt =     "Ich bin " + person.firstName + " " +
+//                 person.familyName +
+//                 " und verdiene " + 
+//                 person.salary[1] + " p.a";
 
-output(txt);
+// output(txt);
 
 
 /***** Objekte 2 - Hierarchie *******/
 
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // Kate Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest
+
+const baikal = {
+                value:"10m",
+                deep:{
+                        deeper:{
+                                deepest: "Das Licht - auf 1642m!"
+                        }
+
+                }
 
 
 
+};
+
+
+output(baikal);
+output(baikal.value);
+output(baikal.deep.deeper.deepest);
 
 /** Ausgabe */
 function output(outputData) {
